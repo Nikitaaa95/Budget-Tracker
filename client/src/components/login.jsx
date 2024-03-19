3633a5cd709ada8fcec10cea3e3e69ca3ddfc757
Nikita Thomas <nikitathomas@Nikitas-MBP.lan>
2024-03-18 22:07:16 -0400

    add true login fields and route to sign up page

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { Form, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
     <div className="container">
       <Form>
+        <Form.Group controlId="formBasicUsername">
+          <Form.Label>Username</Form.Label>
+          <Form.Control type="text" placeholder="Enter username" />
+        </Form.Group>
+
         <Form.Group controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
           <Form.Control type="email" placeholder="Enter email" />
@@ -19,6 +25,9 @@ const Login = () => {
           Submit
         </Button>
       </Form>
+      <div className="mt-3">
+        <p>New User? <Link to="/signup">Sign up!</Link></p>
+      </div>
     </div>
   );
 };
