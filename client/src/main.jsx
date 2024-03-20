c2a07f096911f589ee8d72e227516417b586e516
Nikita Thomas <nikitathomas@Nikitas-MBP.WiFi Extender>
2024-03-20 19:37:56 -0400

    correcting profile issue

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 
 import App from './App.jsx'
 import Home from './pages/Home';
+import Profile from './pages/Profile';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
 import Error from './pages/Error';
@@ -23,8 +24,10 @@ const router = createBrowserRouter([
       }, {
         path: '/signup',
         element: <Signup />
-      }, 
-      {
+      }, {
+        path: '/me',
+        element: <Profile />
+      }, {
         path: '/profiles/:profileId',
         element: <Profile />
       }
