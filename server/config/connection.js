3cec03bd919df8da4392bd768f6d01a00bf0f92e
rosiedeng1 <rd25500@gmail.com>
2024-03-19 23:25:56 -0400

    mongodb

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,8 +2,5 @@ const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/MongoDBBudgetTracker');
 
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, {});
-
 module.exports = mongoose.connection;
 
