854a6155c103ce03be7a613746877149e2467799
rosiedeng1 <rd25500@gmail.com>
2024-03-19 23:13:42 -0400

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,5 +2,8 @@ const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/MongoDBBudgetTracker');
 
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {});
+
 module.exports = mongoose.connection;
 
