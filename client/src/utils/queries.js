a216a6d5ad564c4a6a927d0e51d99e1286acbf96
rosiedeng1 <rd25500@gmail.com>
2024-03-20 22:52:45 -0400

    fixed client side queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,31 +1,47 @@
 import { gql } from '@apollo/client';
 
 export const QUERY_PROFILES = gql`
-  query allProfiles {
-    profiles {
+query Profiles {
+  profiles {
+    _id
+    name
+    email
+    password
+    income {
       _id
-      name
-      skills
+      label
+      amount
     }
   }
+}
 `;
 
 export const QUERY_SINGLE_PROFILE = gql`
-  query singleProfile($profileId: ID!) {
-    profile(profileId: $profileId) {
+query Profile($profileId: ID!) {
+  profile(profileId: $profileId) {
+    _id
+    name
+    email
+    password
+    income {
       _id
-      name
-      skills
+      label
+      amount
     }
   }
+}
 `;
 
 export const QUERY_ME = gql`
-  query me {
-    me {
+query Me {
+  me {
+    _id
+    name
+    email
+    income {
       _id
-      name
-      skills
+      label
+      amount
     }
   }
-`;
+}`;
