c47c18d64495d1b3becef1f565a861d9dd116091
rosiedeng1 <rd25500@gmail.com>
2024-03-21 20:45:20 -0400

    hi

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -3,7 +3,6 @@ import { gql } from '@apollo/client';
 export const ADD_PROFILE = gql`
   mutation addProfile($name: String!, $email: String!, $password: String!) {
     addProfile(name: $name, email: $email, password: $password) {
-      token
       profile {
         _id
         name
@@ -25,7 +24,6 @@ mutation AddIncome($label: String!, $amount: Int!) {
 export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
-      token
       profile {
         _id
         name
